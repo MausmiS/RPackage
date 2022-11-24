3b80a8088d38bb9ef180f731e066656260f9fbb7
MausmiS <113102214+MausmiS@users.noreply.github.com>
2022-11-24 22:03:57 +0530

    My R package

---
+++ R/MausmiS.R
@@ -0,0 +1,14 @@
+#' Square a given number
+#'
+#' Takes any numeric value and returns the square of this number.
+#' @param x A numeric value to be squared.
+#' @return The square of the number
+#' @examples
+#' squared_number <- square(c(5))
+#' squared_number <- square(c(23))
+#' @export
+
+square <- function(x){
+  return(x^2)
+}
+
